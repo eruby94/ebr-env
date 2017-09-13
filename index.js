17eea96834f79d0564b96666ec61bd4a65a22749
Evan Ruby <evanruby94@gmail.com>
2017-09-13 16:38:58 -0400

    Moved scss processing to webpack build on jumpbox

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fireworm = require('fireworm');
 const fw = fireworm('/Users/Evan/nginx', { ignoreInitial: true });
-const scssfw = fireworm('/Users/Evan/nginx/ebr-ui/source/scss/');
 const async = require('async');
 const exec = require('child_process').exec;
 const server = 'ubuntu@ec2-13-59-24-182.us-east-2.compute.amazonaws.com';
@@ -13,8 +12,6 @@ const syncCmd =
 	server +
 	':' +
 	remote_dir;
-const scssCmd =
-	'node-sass -o /Users/Evan/nginx/ebr-ui/source/stylesheets /Users/Evan/nginx/ebr-ui/source/scss/main.scss --output-style compressed';
 var busy = false;
 const sleepTime = 500;
 
@@ -32,30 +29,8 @@ function changed(filename) {
 	}
 }
 
-function scssChanged(filename) {
-	if (!busy) {
-		console.log('\nPre-processing scss files...\n');
-		busy = true;
-
-		exec(scssCmd, function(err, stdout, stderr) {
-			console.log(stdout);
-		});
-
-		setTimeout(function() {
-			busy = false;
-		}, sleepTime);
-	}
-}
-
 async.waterfall(
 	[
-		function(cb) {
-			scssfw.add('**');
-			scssfw.on('add', scssChanged);
-			scssfw.on('change', scssChanged);
-			scssfw.on('remove', scssChanged);
-			cb();
-		},
 		function(cb) {
 			console.log('Creating remote directories...');
 			exec(
@@ -70,7 +45,6 @@ async.waterfall(
 		function(stdout, stderr, cb) {
 			console.log('\n***Starting file sync process***\n');
 			fw.add('**');
-			fw.ignore('ebr-ui/source/scss/');
 			fw.on('add', changed);
 			fw.on('change', changed);
 			fw.on('remove', changed);
